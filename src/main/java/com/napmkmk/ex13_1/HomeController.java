9cfd11b6b2c1afc3c55cad6f58312eb747f0c0cb
ici <ici@DESKTOP-J9CT40H>
2022-10-27 16:54:54 +0900

    데이터 객체 이름 변경 추가기능

--- src/main/java/com/napmkmk/ex13_1/HomeController.java
+++ src/main/java/com/napmkmk/ex13_1/HomeController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -73,14 +74,24 @@ public class HomeController {
 //		
 //		return "joinOk";
 //	}
-	
 
+	
 	@RequestMapping (value = "joinOk")
-	public String joinOk(MemberDto memberDto) { // 맴버 디티오 선언 일반객체 부르기 ★★★객체 이름이 ★클래스★ 이름과 같아야 합니다★★
+	public String joinOk(@ModelAttribute("memberInfo") MemberDto memberDto) { // @ModelAttribute 쓰면 데이터 객체 이름을 원하는걸로 변경가능
 		
 		
 		return "joinOk";
 	}
+	
+	
+	
+//
+//	@RequestMapping (value = "joinOk")
+//	public String joinOk(MemberDto memberDto) { // 맴버 디티오 선언 일반객체 부르기 ★★★객체 이름이 ★클래스★ 이름과 같아야 합니다★★
+//		
+//		
+//		return "joinOk";
+//	}
 	
 	@RequestMapping(value = "/height/{memberHT}")
 	public String memberHeight(@PathVariable String memberHT, Model model) {
--- src/main/webapp/WEB-INF/views/joinOk.jsp
+++ src/main/webapp/WEB-INF/views/joinOk.jsp
@@ -6,12 +6,23 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
+
+
 <body>
+
+	아이디 : ${memberInfo.id } <br>
+	비밀번호 : ${memberInfo.pw } <br>
+	이름 : ${memberInfo.name } <br>
+	이메일 : ${memberInfo.email } <br>
+	
+	
+	
+<!--  
 	아이디 : ${memberDto.id } <br>
 	비밀번호 : ${memberDto.pw } <br>
 	이름 : ${memberDto.name } <br>
 	이메일 : ${memberDto.email } <br>
-	
+-->	
 <!--  
 	아이디 : ${id } <br>
 	비밀번호 : ${pw } <br>
